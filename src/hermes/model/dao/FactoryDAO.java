81307fc3c5c6a55ec17f339bdd6d1e36f0983b29
Matías Celasco <matiascelasco@gmail.com>
2015-11-21 03:38:58 -0300

    enum DAOs

--- src/hermes/model/dao/FactoryDAO.java
+++ src/hermes/model/dao/FactoryDAO.java
@@ -2,7 +2,12 @@ package hermes.model.dao;
 
 import hermes.model.Notification;
 import hermes.model.Tag;
+import hermes.model.enums.Category;
+import hermes.model.enums.Content;
+import hermes.model.enums.Context;
+import hermes.model.enums.Kid;
 import dao.DAO;
+import dao.EnumDAO;
 
 public class FactoryDAO {
 
@@ -10,10 +15,19 @@ public class FactoryDAO {
 	private static ConnectionWrapper connectionWrapper = new ConnectionWrapper();
 	private static NotificationDAOforJDBC notificationDAO = new NotificationDAOforJDBC();
 	private static TagDAOforJDBC tagDAO = new TagDAOforJDBC();
+	private static EnumDAO<Kid> kidDAO = new EnumDAO<Kid>(Kid.class);
+	private static EnumDAO<Context> contextDAO = new EnumDAO<Context>(Context.class);
+    private static EnumDAO<Content> contentDAO = new EnumDAO<Content>(Content.class);
+    private static EnumDAO<Category> categoryDAO = new EnumDAO<Category>(Category.class);
+	
 	
 	static {
 		notificationDAO.setConnectionWrapper(connectionWrapper);
 		tagDAO.setConnectionWrapper(connectionWrapper);
+//		kidDAO.setConnectionWrapper(connectionWrapper);
+//		contextDAO.setConnectionWrapper(connectionWrapper);
+//		contentDAO.setConnectionWrapper(connectionWrapper);
+//		categoryDAO.setConnectionWrapper(connectionWrapper);
 		connectionWrapper.prepare();
 	}
 	
@@ -24,5 +38,23 @@ public class FactoryDAO {
 	public static DAO<Tag> getTagDAO() {
 		return tagDAO;
 	}
+	
+	public static DAO<Kid> getKidDAO() {
+		return kidDAO;
+	}
+	
+	public static DAO<Content> getContentDAO() {
+		return contentDAO;
+	}
+	
+	public static DAO<Context> getContextDAO() {
+		return contextDAO;
+	}
+	
+	public static DAO<Category> getCategoryDAO() {
+		return categoryDAO;
+	}
+	
+	
 
 }
