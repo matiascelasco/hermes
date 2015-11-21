7ee17654c6bb31937df74b86ae3cf36a5c16bf89
Matías Celasco <matiascelasco@gmail.com>
2015-11-21 13:08:37 -0300

    properties file

--- src/hermes/monitor/Main.java
+++ src/hermes/monitor/Main.java
@@ -1,16 +1,32 @@
 package hermes.monitor;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import hermes.model.Model;
 import hermes.monitor.view.View;
 
-//TODO: la relación entre tags y notifications debería ser many to many
-//TODO: problemas con la tabla de notificacaiones al achivar el frame
+//TODO: problemas con la tabla de notificacaiones al achicar el frame
 //TODO: el ordenamiento de la columna de fecha no anda
 //TODO: que los fields de fecha se actualizen automáticamente de manera tal que Desde nunca sea mayor que Hasta
 //TODO: ver si se puede usar las fechas que hay en java.time en vez de las de utils en el combo box
-//TODO: mencionar como comentario en la entrega la version de java (8) y el compilation level (1.6 en vez de 1.4)
 
 public class Main {
+	
+	
+	public static final Properties properties;
+	static {
+		properties = new Properties();
+		InputStream inputStream = Main.class.getResourceAsStream("/config.properties");
+		try {
+			properties.load(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 	
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
